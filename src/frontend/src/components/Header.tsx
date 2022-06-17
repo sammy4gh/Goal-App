f2f747abfb2d8192edcf36918ee4a8f0723be2d7
samuel <aristocrat4gh@gmail.com>
2022-06-17 01:14:28 +0000

    save

--- src/frontend/src/components/Header.tsx
+++ src/frontend/src/components/Header.tsx
@@ -1,9 +1,6 @@
-import { FC } from "react";
 import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
 import CustomButton from "./CustomButton";
 import LoginIcon from "@mui/icons-material/Login";
 import LogoutIcon from "@mui/icons-material/Logout";
